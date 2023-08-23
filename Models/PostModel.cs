02eabdb0506f6f156f2e60e5d8b5a20be9a382d6
alicanaksoy <alicanaksoy00@gmail.com>
2023-08-23 10:07:50 +0300

    initial

---
+++ Models/PostModel.cs
@@ -0,0 +1,17 @@
+namespace blog_application.Models{
+
+    public class PostModel
+        {
+            public int Id { get; set; }
+
+            public string Title { get; set; }
+
+            public string Body{ get; set; }
+
+            public DateTime CreatedAt { get; set; } 
+
+            public DateTime UpdatedAt { get; set; } 
+        }
+}
+
+     
---
+++ Models/ViewModels/PostViewModel.cs
@@ -0,0 +1,10 @@
+ namespace blog_application.Models.ViewModels;
+
+public class PostViewModel 
+{
+    public List<PostModel> PostList { get; set; }   
+
+    public PostModel Post { get; set; } 
+
+    
+}
